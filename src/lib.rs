674a7971b4b9a33863434bc78d24f7206744ee3c
Max Kagamine <max@kgmn.jp>
2025-03-15 13:31:32 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright (c) Max Kagamine
+// Licensed under the Apache License, Version 2.0
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
