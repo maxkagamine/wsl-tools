93df58317f18a14e3384ebd59a353205f3a0e8f7
Max Kagamine <max@kgmn.jp>
2025-09-18 22:16:28 +0900

    Improved command parsing

--- src/bin/code-wsl.rs
+++ src/bin/code-wsl.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Max Kagamine
 // Licensed under the Apache License, Version 2.0
 
-#![windows_subsystem = "windows"]
+#![cfg_attr(not(test), windows_subsystem = "windows")]
 
 use clap::Parser;
 
@@ -77,7 +77,6 @@ fn main() {
 
 #[cfg(windows)]
 fn get_vscode_exe() -> anyhow::Result<String> {
-    use anyhow::anyhow;
     use windows::{
         Win32::{
             Foundation::{ERROR_MORE_DATA, ERROR_SUCCESS},
@@ -108,10 +107,7 @@ fn get_vscode_exe() -> anyhow::Result<String> {
                 }
                 let length = buffer.iter().position(|&x| x == 0).unwrap_or(buffer.len());
                 let value = String::from_utf16(&buffer[..length])?;
-                return match value.split('"').find(|x| !x.is_empty()) {
-                    Some(x) => Ok(x.to_string()),
-                    None => Err(anyhow!("Unexpected value in registry.")),
-                };
+                return Ok(exe_from_command_string(&value));
             }
             ERROR_MORE_DATA => {
                 buffer.reserve((buffer_size as usize / 2) - buffer.len());
@@ -120,6 +116,16 @@ fn get_vscode_exe() -> anyhow::Result<String> {
                 return Err(Error::from_hresult(err.to_hresult()).into());
             }
         }
+    }
+}
+
+#[cfg(windows)]
+fn exe_from_command_string(command: &str) -> String {
+    let trimmed = command.trim_start();
+    if trimmed.starts_with('"') {
+        trimmed.chars().skip(1).take_while(|x| *x != '"').collect()
+    } else {
+        trimmed.chars().take_while(|x| *x != ' ').collect()
     }
 }
 
@@ -134,4 +140,22 @@ fn get_distro_name() -> anyhow::Result<String> {
         .arg("$WSL_DISTRO_NAME")
         .output()?;
     Ok(String::from_utf8(output.stdout)?.trim().to_string())
+}
+
+#[cfg(all(test, windows))]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exe_from_command_string_works() {
+        assert_eq!(
+            exe_from_command_string(r#"C:\Windows\notepad.exe "%1""#),
+            r"C:\Windows\notepad.exe"
+        );
+
+        assert_eq!(
+            exe_from_command_string(r#" "C:\Program Files\Foo\bar.exe" /x "%1" "#),
+            r"C:\Program Files\Foo\bar.exe"
+        );
+    }
 }
